ba6ae67a6ca5fa08dbc24b8e6546da433d02a675
Steve Lee <imslee100@gmail.com>
2022-01-24 00:48:31 +0900

    Read Data

--- index.js
+++ index.js
@@ -4,24 +4,40 @@ const { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
+    type Product {
+        id: ID!
+        name: String
+        price: Int
+        description : String
+    }
     type Query {
-        hello: String,
-        nodejs: Int
+        getProduct ( id: ID! ): Product
     }
 `);
 
+const products = [{
+    id: 1,
+    name: '첫번째 제품',
+    price: 2000,
+    description : "첫번째 제품입니다."
+},{
+    id: 2,
+    name: '두번째 제품',
+    price: 1200,
+    description : "두번째 제품입니다."
+}]
+
 const root = { 
-    hello: () => 'Hello world!' ,
-    nodejs: () => 20 ,
+    getProduct : ({id}) => products.find( product => product.id === parseInt(id) )
 };
 
 const app = express();
 app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
-
-app.listen(4000, () => {
-  console.log('Running a GraphQL API server at localhost:4000/graphql');
-}); 
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  }));
+  
+  app.listen(4000, () => {
+    console.log('Running a GraphQL API server at localhost:4000/graphql');
+  }); 
